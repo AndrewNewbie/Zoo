1e8e919b60c1666945b01663859c2174ff481f52
Андрій Просвітлюк <prosvitlyukandrew@gmail.com>
2024-05-27 01:47:53 +0300

    Commit

--- Zoo/src/main/java/org/example/Animal.java
+++ Zoo/src/main/java/org/example/Animal.java
@@ -1,12 +1,15 @@
 package org.example;
 
 public class Animal {
-    protected int paws;
-    protected String sound;
-    protected String name;
+    public int paws;
+    public String sound;
+    public String name;
 
     public void getValues() {
         String a = "Ви створили тварину "+this.name+", у нього "+this.paws+" лапи і він видає звук "+this.sound;
         System.out.println(a);
+    }
+    public String getSound(){
+        return this.sound;
     }
 }
--- Zoo/src/main/java/org/example/Main.java
+++ Zoo/src/main/java/org/example/Main.java
@@ -14,8 +14,7 @@ public class Main {
         Pelican pelican = new Pelican();
 
 
-        ArrayList<String> animals = new ArrayList<>();
-        ArrayList<String> sounds = new ArrayList<>();
+        ArrayList<Animal> animalsList = new ArrayList<>();
         int numberPaws = 0;
 
         System.out.println("Введіть кількість тварин:");
@@ -23,86 +22,84 @@ public class Main {
         for (int i = 0; i < numberAnimals; i++) {
             System.out.println("Виберіть тип тварини, яку хочете додати:1-Cat, 2-Spider, 3-HoneyEater, 4-Octopus, 5-Pelican:");
             int typeAnimal = scanner.nextInt();
-            if (typeAnimal == 1) {
+            switch (typeAnimal) {
+                case 1:
+                    System.out.println("Ви обрали кота, введіть кількість лап:");
+                    int paws = scanner.nextInt();
+                    System.out.println("Який звук видає кіт:");
+                    scanner.nextLine();
+                    String sound = scanner.nextLine();
 
-                System.out.println("Ви обрали кота, введіть кількість лап:");
-                int paws = scanner.nextInt();
-                System.out.println("Який звук видає кіт:");
-                scanner.nextLine();
-                String sound = scanner.nextLine();
+                    cat.setValues(paws, sound, "Cat");
+                    cat.getValues();
+                    System.out.println("Введіть наступну тварину");
 
-                cat.setValues(paws, sound, "Cat");
-                cat.getValues();
-                System.out.println("Введіть наступну тварину");
+                    animalsList.add(cat);
+                    numberPaws = +cat.paws;
+                    break;
+                case 2:
+                    System.out.println("Ви обрали павука, введіть кількість лап:");
+                    paws = scanner.nextInt();
+                    System.out.println("Який звук видає паук:");
+                    scanner.nextLine();
+                    sound = scanner.nextLine();
+                    spider.setValues(paws, sound, "Spider");
+                    spider.getValues();
+                    System.out.println("Введіть наступну тварину");
 
-                animals.add(i, "Cat");
-                sounds.add(i, cat.sound);
-                numberPaws = +cat.paws;
-            }
-            if (typeAnimal == 2) {
-                System.out.println("Ви обрали павука, введіть кількість лап:");
-                int paws = scanner.nextInt();
-                System.out.println("Який звук видає паук:");
-                scanner.nextLine();
-                String sound = scanner.nextLine();
-                spider.setValues(paws, sound, "Spider");
-                spider.getValues();
-                System.out.println("Введіть наступну тварину");
+                    animalsList.add(spider);
+                    numberPaws = +cat.paws;
+                    break;
+                case 3:
+                    System.out.println("Ви обрали медоїда, введіть кількість лап:");
+                    paws = scanner.nextInt();
+                    System.out.println("Який звук видає медоїд:");
+                    scanner.nextLine();
+                    sound = scanner.nextLine();
+                    honeyEater.setValues(paws, sound, "HoneyEater");
+                    honeyEater.getValues();
+                    System.out.println("Введіть наступну тварину");
 
-                animals.add(i, "Spider");
-                sounds.add(i, spider.sound);
-                numberPaws = +cat.paws;
-            }
-            if (typeAnimal == 3) {
-                System.out.println("Ви обрали медоїда, введіть кількість лап:");
-                int paws = scanner.nextInt();
-                System.out.println("Який звук видає медоїд:");
-                scanner.nextLine();
-                String sound = scanner.nextLine();
-                honeyEater.setValues(paws, sound, "HoneyEater");
-                honeyEater.getValues();
-                System.out.println("Введіть наступну тварину");
+                    animalsList.add(honeyEater);
+                    numberPaws = +honeyEater.paws;
+                    break;
+                case 4:
+                    System.out.println("Ви обрали восьминога, введіть кількість лап:");
+                    paws = scanner.nextInt();
+                    System.out.println("Який звук видає Восьминіг:");
+                    scanner.nextLine();
+                    sound = scanner.nextLine();
+                    octopus.setValues(paws, sound, "Octopus");
+                    octopus.getValues();
+                    System.out.println("Введіть наступну тварину");
 
-                animals.add(i, "HoneyEater");
-                sounds.add(i, honeyEater.sound);
-                numberPaws = +honeyEater.paws;
-            }
-            if (typeAnimal == 4) {
-                System.out.println("Ви обрали восьминога, введіть кількість лап:");
-                int paws = scanner.nextInt();
-                System.out.println("Який звук видає Восьминіг:");
-                scanner.nextLine();
-                String sound = scanner.nextLine();
-                octopus.setValues(paws, sound, "Octopus");
-                octopus.getValues();
-                System.out.println("Введіть наступну тварину");
+                    animalsList.add(octopus);
+                    numberPaws = +octopus.paws;
+                    break;
+                case 5:
+                    System.out.println("Ви обрали пелікана, введіть кількість лап:");
+                    paws = scanner.nextInt();
+                    System.out.println("Який звук видає пелікан:");
+                    scanner.nextLine();
+                    sound = scanner.nextLine();
+                    pelican.setValues(paws, sound, "Pelican");
+                    pelican.getValues();
+                    System.out.println("Введіть наступну тварину");
 
-                animals.add(i, "Octopus");
-                sounds.add(i, octopus.sound);
-                numberPaws = +octopus.paws;
-            }
-            if (typeAnimal == 5) {
-                System.out.println("Ви обрали пелікана, введіть кількість лап:");
-                int paws = scanner.nextInt();
-                System.out.println("Який звук видає пелікан:");
-                scanner.nextLine();
-                String sound = scanner.nextLine();
-                pelican.setValues(paws, sound, "Pelican");
-                pelican.getValues();
-                System.out.println("Введіть наступну тварину");
-
-                animals.add(i, "Pelican");
-                sounds.add(i, pelican.sound);
-                numberPaws = +pelican.paws;
+                    animalsList.add(pelican);
+                    numberPaws = +pelican.paws;
+                    break;
             }
         }
         System.out.println("Ви ввели усіх тварин, виберіть опцію зоопарку\n1 - Послухати, 2 - Порахувати кількість усіх лап: ");
         int oneTwo = scanner.nextInt();
         if (oneTwo == 1) {
-            for (int i = 0; i < numberAnimals; i++) {
-                System.out.println(animals.get(i)+" видає звук -"+sounds.get(i));
+            for (Animal el : animalsList) {
+                System.out.println(el.name + " видає звук -" + el.getSound());
             }
-        } else
+        }
+        if (oneTwo == 2) {
             System.out.println("кількість усіх лап:" + numberPaws);
+        }
     }
 }
