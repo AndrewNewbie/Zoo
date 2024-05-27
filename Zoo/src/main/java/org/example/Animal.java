777d898f66b662c03800620f0e7ac74d7cdb945a
Андрій Просвітлюк <prosvitlyukandrew@gmail.com>
2024-05-27 03:01:01 +0300

    Commit

--- Zoo/src/main/java/org/example/Animal.java
+++ Zoo/src/main/java/org/example/Animal.java
@@ -1,15 +1,26 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Animal {
     public int paws;
     public String sound;
     public String name;
 
-    public void getValues() {
-        String a = "Ви створили тварину "+this.name+", у нього "+this.paws+" лапи і він видає звук "+this.sound;
+    public void printCreatedAnimal() {
+        String a = "Ви створили тварину " + this.name + ", у нього " + this.paws + " лапи і він видає звук " + this.sound;
         System.out.println(a);
     }
-    public String getSound(){
+
+    public String getSound() {
         return this.sound;
+    }
+
+    public static int getPaws(ArrayList<Animal> animalsList) {
+        int SumaPaws = 0;
+        for (Animal animal : animalsList) {
+            SumaPaws += animal.paws;
+        }
+        return SumaPaws;
     }
 }
--- Zoo/src/main/java/org/example/Main.java
+++ Zoo/src/main/java/org/example/Main.java
@@ -1,7 +1,118 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
+    static ArrayList<Animal> animalsList = new ArrayList<>();
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Введіть кількість тварин:");
+        int numberAnimals = scanner.nextInt();
+        for (int i = 0; i < numberAnimals; i++) {
+            System.out.println("Виберіть тип тварини, яку хочете додати:1-Cat, 2-Spider, 3-HoneyEater, 4-Octopus, 5-Pelican:");
+            int typeAnimal = scanner.nextInt();
+            switch (typeAnimal) {
+                case 1:
+                    animalCat();
+                    break;
+                case 2:
+                    animalSpider();
+                    break;
+                case 3:
+                    animalHoneyEater();
+                    break;
+                case 4:
+                    animalOctopus();
+                    break;
+                case 5:
+                    animalPelican();
+                    break;
+            }
+        }
+        System.out.println("Ви ввели усіх тварин, виберіть опцію зоопарку\n1 - Послухати, 2 - Порахувати кількість усіх лап: ");
+        int SelectOptions = scanner.nextInt();
+        if (SelectOptions == 1) {
+            for (Animal el : animalsList) {
+                System.out.println(el.name + " видає звук -" + el.getSound());
+            }
+        }
+        if (SelectOptions == 2) {
+            System.out.println("кількість усіх лап:" + Animal.getPaws(animalsList));
+        }
+    }
+
+    public static void animalCat() {
+        Cat cat = new Cat();
+        System.out.println("Ви обрали кота, введіть кількість лап:");
+        int paws = scanner.nextInt();
+        System.out.println("Який звук видає кіт:");
+        scanner.nextLine();
+        String sound = scanner.nextLine();
+
+        cat.setValues(paws, sound, "Cat");
+        cat.printCreatedAnimal();
+        System.out.println("Введіть наступну тварину");
+
+        animalsList.add(cat);
+    }
+
+    public static void animalSpider() {
+        Spider spider = new Spider();
+        System.out.println("Ви обрали павука, введіть кількість лап:");
+        int paws = scanner.nextInt();
+        System.out.println("Який звук видає паук:");
+        scanner.nextLine();
+        String sound = scanner.nextLine();
+        spider.setValues(paws, sound, "Spider");
+        spider.printCreatedAnimal();
+        System.out.println("Введіть наступну тварину");
+
+        animalsList.add(spider);
+
+    }
+
+    public static void animalHoneyEater() {
+        HoneyEater honeyEater = new HoneyEater();
+        System.out.println("Ви обрали медоїда, введіть кількість лап:");
+        int paws = scanner.nextInt();
+        System.out.println("Який звук видає медоїд:");
+        scanner.nextLine();
+        String sound = scanner.nextLine();
+        honeyEater.setValues(paws, sound, "HoneyEater");
+        honeyEater.printCreatedAnimal();
+        System.out.println("Введіть наступну тварину");
+
+        animalsList.add(honeyEater);
+    }
+
+    public static void animalOctopus() {
+        Octopus octopus = new Octopus();
+
+        System.out.println("Ви обрали восьминога, введіть кількість лап:");
+        int paws = scanner.nextInt();
+        System.out.println("Який звук видає Восьминіг:");
+        scanner.nextLine();
+        String sound = scanner.nextLine();
+        octopus.setValues(paws, sound, "Octopus");
+        octopus.printCreatedAnimal();
+        System.out.println("Введіть наступну тварину");
+
+        animalsList.add(octopus);
+    }
+
+    public static void animalPelican() {
+        Pelican pelican = new Pelican();
+        System.out.println("Ви обрали пелікана, введіть кількість лап:");
+        int paws = scanner.nextInt();
+        System.out.println("Який звук видає пелікан:");
+        scanner.nextLine();
+        String sound = scanner.nextLine();
+        pelican.setValues(paws, sound, "Pelican");
+        pelican.printCreatedAnimal();
+        System.out.println("Введіть наступну тварину");
+
+        animalsList.add(pelican);
     }
 }
